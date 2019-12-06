6212e974f672fbb847cf09ec511f42505276daf1
Jevil36239 <51107256+Jevil36239@users.noreply.github.com>
2019-12-06 14:29:06 +0700

    Add files via upload

---
+++ Install.py
@@ -0,0 +1,17 @@
+import os,sys,time
+
+os.system('clear')
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+babi('\x1b[34m[\x1b[92m+\x1b[34m] \x1b[96mMenginstall Bahan \x1b[34m[\x1b[92m+\x1b[34m]')
+os.system('sh abshdvshsahsvwhz')
+
+exit()
+
+
+
